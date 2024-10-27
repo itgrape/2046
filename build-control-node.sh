29919bb7a9a83cdebae3192f9a34a79c36ac3422
Grape <itgrape@outlook.com>
2024-10-27 10:52:43 +0800

    optimize containerfile

--- build-control-node.sh
+++ build-control-node.sh
@@ -8,8 +8,8 @@ echo "=== build openhpc"
 $CONTAINER build -t ohpc-container/openhpc:3 -f openhpc/Containerfile openhpc \
   --build-arg ARCH=$ARCH
 
-# echo "=== build head"
-# $CONTAINER build -t ohpc-container/head -f head/Containerfile head
+echo "=== build head"
+$CONTAINER build -t ohpc-container/head -f head/Containerfile head
 
 
 $CONTAINER image prune -f

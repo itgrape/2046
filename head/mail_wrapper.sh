f755ddc64459c08a541be206f29a3fa60e14794a
Grape <itgrape@outlook.com>
2024-10-28 11:19:20 +0800

    optimize email

---
+++ head/mail_wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+JOB_ID=$SLURM_JOB_ID
+JOB_NAME=$SLURM_JOB_NAME
+JOB_USER=$SLURM_JOB_USER
+JOB_STATE=$SLURM_JOB_STATE
+MAIL_TYPE=$SLURM_JOB_MAIL_TYPE
+MAIL_USER=$(scontrol show job $JOB_ID | grep -oP '(?<=MailUser=)[^ ]*')
+
+
+SUBJECT="$JOB_ID - $JOB_NAME - $MAIL_TYPE"
+BODY="用户名称: $JOB_USER\n作业ID: $JOB_ID\n作业名称: $JOB_NAME\n作业状态: $JOB_STATE\n\n"
+
+echo -e "To: $MAIL_USER\nSubject: $SUBJECT\n\n$BODY" | /usr/sbin/sendmail -v -t >> /var/log/slurm/sendmail.log

d5e9f6b9b8caa7ebdf106d8e501ad78b06ff48da
Grape <itgrape@outlook.com>
2024-11-26 21:37:15 +0800

    user_recover

--- head/custom_script/recover_auth_files.sh
+++ head/custom_script/recover_auth_files.sh
@@ -1,2 +1,9 @@
-\cp -rf /srv/salt/recover/* /etc/
+#!/bin/bash
+
+cat /tmp/auth_files/passwd.bak  >> /etc/passwd
+cat /tmp/auth_files/group.bak  >> /etc/group
+cat /tmp/auth_files/gshadow.bak  >> /etc/gshadow
+cat /tmp/auth_files/shadow.bak  >> /etc/shadow
+\cp -rf /tmp/auth_files/login.defs /etc/login.defs
+
 salt '*' state.apply sync_auth_files

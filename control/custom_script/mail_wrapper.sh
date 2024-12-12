d21e853b46ee58f6944ae0fc67f1847fa160e96c
Grape <itgrape@outlook.com>
2024-12-12 12:24:21 +0800

    mail

--- control/custom_script/mail_wrapper.sh
+++ control/custom_script/mail_wrapper.sh
@@ -11,4 +11,16 @@ MAIL_USER=$(scontrol show job $JOB_ID | grep -oP '(?<=MailUser=)[^ ]*')
 SUBJECT="$JOB_ID - $JOB_NAME - $MAIL_TYPE"
 BODY="用户名称: $JOB_USER\n作业ID: $JOB_ID\n作业名称: $JOB_NAME\n作业状态: $JOB_STATE\n\n"
 
-echo -e "To: $MAIL_USER\nSubject: $SUBJECT\n\n$BODY" | /usr/sbin/sendmail -v -t >> /var/log/slurm/sendmail.log
+# echo -e "To: $MAIL_USER\nSubject: $SUBJECT\n\n$BODY" | /usr/sbin/sendmail -v -t >> /var/log/slurm/sendmail.log
+
+curl -X POST http://api.pushihao.com/v1/email -H "Content-Type: application/json" -d << EOF
+{
+    "secretKey":"pushihao",
+    "fromName": "Slurm",
+    "fromEmail": "imag@njust.edu.cn",
+    "subject": "$SUBJECT",
+    "htmlContent": "$BODY",
+    "toName": "Dear $JOB_USER",
+    "toEmail": "$MAIL_USER"
+}
+EOF

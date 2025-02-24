f9963ab6a054502825789e91556622d9b341326e
itgrape <itgrape@outlook.com>
2025-02-24 20:54:02 +0800

    add lock for denied_users

--- compute/custom_script/prolog.sh
+++ compute/custom_script/prolog.sh
@@ -3,15 +3,18 @@
 echo "Prolog script started at $(date) for User ${SLURM_JOB_USER} Job ${SLURM_JOB_ID}" >> /var/log/slurm/prolog.log
 
 
+# 通过互斥锁确保同一时间文件只被一个进程访问
+{
+    flock -x 200
 
-JOB_INFO=$(squeue -u "$SLURM_JOB_USER" -o "%P" -h)
-if echo "$JOB_INFO" | grep -q "Debug" || [[ "$SLURM_JOB_PARTITION" == "Debug" ]]; then
-    # 当前用户有 Debug 分区的作业
-    sed -i "\|${SLURM_JOB_USER}|d" /etc/ssh/denied_users
-else
-    echo "$SLURM_JOB_USER" >> /etc/ssh/denied_users
-fi
-
+    JOB_INFO=$(squeue -u "$SLURM_JOB_USER" -o "%P" -h)
+    if echo "$JOB_INFO" | grep -q "Debug" || [[ "$SLURM_JOB_PARTITION" == "Debug" ]]; then
+        # 当前用户有 Debug 分区的作业
+        sed -i "\|${SLURM_JOB_USER}|d" /etc/ssh/denied_users
+    else
+        echo "$SLURM_JOB_USER" >> /etc/ssh/denied_users
+    fi
+} 200>/tmp/denied_users.lock
 
 
 unit_name="check_service_${SLURM_JOB_USER}_${SLURM_JOB_ID}"
